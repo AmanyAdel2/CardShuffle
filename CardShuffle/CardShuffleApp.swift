371e2a94251d65ce4351297e7ac17c25c4881613
AmanyAdel2 <amanyadel20242024@gmail.com>
2026-08-30 09:54:42 +0300

    Initial Commit

---
+++ CardShuffle/CardShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardShuffleApp.swift
+//  CardShuffle
+//
+//  Created by Macos on 30/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct CardShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
